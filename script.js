44df60e4994f512299300744d7d0882dcc87a026
Ryan Cawley <ryan.cawley@outlook.com>
2018-11-21 15:23:29 -0700

    Updating JS, Testing for input values to show back

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ function idCard() {
   //get reference to DOM elements
   var firstName = document.getElementById("firstName").value;
   var lastName = document.getElementById("lastName").value;
-  var idAddress = document.getElementById("idAddress").value;
+  var idAge = document.getElementById("age").value;
+  var phoneNumber = document.getElementById("phoneNumber").value;
+  var idAddress = document.getElementById("address").value;
 
   var fullName = firstName + " " + lastName;
+
+  document.getElementById("postAddress").innHTML = idAddress;
+  document.getElementById("postAge").innerHTML = idAge;
+  document.getElementById("postPhoneNumber").innerHTML = phoneNumber;
   document.getElementById("postFullName").innerHTML = fullName;
-  var idAddress = document.getElementById("postAddress");
 
-  var idAges = [100, 9000];
-  idAges.push(idAge);
+  // var idAges = [100, 9000];
+  //idAges.push(idAge);
 
-  for (var i = 0; i < idAges.length; i++) {
-    if (parseInt(idAges[i]) < 100) {
-      idAgeOutput.innerHTML = idAges[i];
-    }
-  }
+  //for (var i = 0; i < idAges.length; i++) {
+  //if (parseInt(idAges[i]) < 100) {
+  //idAgeOutput.innerHTML = idAges[i];
+  //}
+  //}
 }
